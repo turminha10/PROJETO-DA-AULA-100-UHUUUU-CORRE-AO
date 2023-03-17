822c48d1dc78a4658be52279d153ced841a898be
turminha10 <116770516+turminha10@users.noreply.github.com>
2023-03-17 11:00:15 -0300

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBshqpYPd7YzSzCGEHprJ09b80RRX5NhtA",
+  authDomain: "projeto-de-conversar.firebaseapp.com",
+  databaseURL: "https://projeto-de-conversar-default-rtdb.firebaseio.com",
+  projectId: "projeto-de-conversar",
+  storageBucket: "projeto-de-conversar.appspot.com",
+  messagingSenderId: "612351816631",
+  appId: "1:612351816631:web:0a8e119a6c4bfa7b7a2e32",
+  measurementId: "G-5N6B6HQ7HV"
+};
